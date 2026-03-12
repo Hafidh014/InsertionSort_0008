ed1d26bd07af63d67d841c76cff5575e47a9a828
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-03-12 15:11:07 +0700

    digunakan untuk menampilkan array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -44,3 +44,24 @@ void insertionsort() {
         }
     }
 }       
+
+void display() 
+{
+    cout << endl;
+    cout << "\n======================";
+    cout << "\nElement Array" << endl;
+    cout << "\n======================";
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    input();
+    insertionsort();
+    display();
+    return 0;
+}
