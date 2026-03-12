555c45a4f874cad8c9fcf2c23d3585de5eb3ad45
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-03-12 15:07:18 +0700

    untuk memasukan data dalam array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -15,3 +15,14 @@ void input() {
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
     }     
+
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "   Masukan Elemen Array   " << endl;
+    cout << "=====================" << endl;
+    
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
